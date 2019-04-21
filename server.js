0b1a1e7f1fe353ccf9e00d4e5f72a64680ab932d
Marcus Thelin <marcusthelin97@gmail.com>
2019-04-21 19:37:35 +0200

    Dockerized the server

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const prerender = require('prerender');
-const server = prerender({ port: process.env.PORT || 9000 });
+const server = prerender({
+    port: process.env.PORT || 9000,
+    chromeLocation: process.env.CHROME_LOCATION || null,
+    chromeFlags: !process.env.DEBUG && ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+});
 process.env.CACHE && server.use(require('prerender-memory-cache'));
+process.env.CACHE && console.log('Using cache. Remember! This is an in-memory-cache.')
+console.log('Chromium location:', process.env.CHROME_LOCATION)
 server.start();
